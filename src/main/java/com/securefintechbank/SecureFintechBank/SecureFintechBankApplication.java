c3419995b590621f9d098a24f0777784e8182adb
madhavivaghela <madhavivaghela1@gmail.com>
2026-03-19 18:48:04 -0400

    Connected to database

---
+++ src/main/java/com/securefintechbank/SecureFintechBank/SecureFintechBankApplication.java
@@ -0,0 +1,13 @@
+package com.securefintechbank.SecureFintechBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureFintechBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureFintechBankApplication.class, args);
+	}
+
+}
